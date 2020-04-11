270a4c80df6aed0f20646c588ab1e29cfb219bc8
Sirwali Joe <mulavhe@gmail.com>
2020-04-12 01:15:35 +0200

    Point to the correct port number

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
  docker stop jmr-squared-website
  docker rm jmr-squared-website
  docker build -t jmr-squared-website:latest .
- docker run -dit -p 9991:3000 --restart=always --name jmr-squared-website jmr-squared-website:latest
+ docker run -dit -p 9991:8080 --restart=always --name jmr-squared-website jmr-squared-website:latest
